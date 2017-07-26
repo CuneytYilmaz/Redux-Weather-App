8f1cc2ab9491d00df750da076a84419635714847
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-26 21:00:48 +0300

    axios js added, ajax request created

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from "axios";
+
 const API_KEY = "d8e94e62b0a5ddae62c10326f3377b76";
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export default function fecthWeather(city) {
+  const url = `${ROOT_URL}&q=${city},tr`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
